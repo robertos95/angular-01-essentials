8bc395683e1e67d79344a7b24088a02107d7d933
Roberto <salimroberto95@gmail.com>
2024-12-30 14:18:46 +0800

    feat: creating and using custom component

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  // angular 19 or higher, standalone: true is by default
+  // standalone: true,
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
