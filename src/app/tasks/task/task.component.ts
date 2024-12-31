20fb9dcbe3c20a2a29d67a26b6d685aafd058199
Roberto <salimroberto95@gmail.com>
2024-12-31 22:00:49 +0800

    refactor: use services more and less event emitter

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CardComponent } from '../../shared/card/card.component';
+import { TasksService } from '../tasks.service';
 import { Task } from './task.model';
 
 @Component({
@@ -11,9 +12,9 @@ import { Task } from './task.model';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  private tasksService = inject(TasksService);
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -20,10 +20,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {
-    this.tasksService.removeTask(taskId);
-  }
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
