1e0c675f80f6ef97c795c51b486c17dbb7fbf445
Roberto <salimroberto95@gmail.com>
2024-12-31 22:27:07 +0800

    feat: convert AppComponent into Angular module, and bootstrap it

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+// bootstrapping standalone Angular application
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err)
+// );
+
+// bootstrapping Angular application with Angular modules (or mixture of it and standalone components)
+platformBrowserDynamic().bootstrapModule(AppModule);
